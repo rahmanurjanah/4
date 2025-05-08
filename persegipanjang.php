a7e85b8b34af041386e56ad3dc929d2d85c66d7c
rahmanurjannah <rahmanurjanah1705@gmail.com>
2025-05-08 09:58:35 +0700

    init

---
+++ persegipanjang.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<a href="index.php">Home</a>
+    <h1>Persegi panjang</h1>
+    <form action="persegipanjang.php" method="post">
+        Masukan panjang 
+        <input type="number" name="Panjang" id=""><br>
+        Masukan lebar
+        <input type="number" name="Lebar" id=""><br>
+       <input type="submit" value="Hitung luas dan keliling">
+    </form>
+</body>
+</html>
